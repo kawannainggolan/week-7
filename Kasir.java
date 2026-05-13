af039dba323d1754c21adf1a4ce6a97949156aa6
kawannainggolan <kawannainggolan19@gmail.com>
2026-05-13 10:30:47 +0700

    menambahkan metod tampilIdentitas dan prosesBelanja

--- Kasir.java
+++ Kasir.java
@@ -5,4 +5,20 @@ public class Kasir {
         this.namaKasir = namaKasir;
     }
     
+    public void tampilkIndentitas() {
+        System.out.println("==== TOKO GARUDA =====");
+        System.out.println("Nama Kasir: " + namaKasir);
+    }
+
+    public void prosesBelanja(Produk p, int jmlhBeli) {
+        if (p.getStok() < jmlhBeli) {
+            System.out.println("Out of stok");
+        }
+        {
+            double hargaAkhir = p.hitungHargaAkhir(jmlhBeli);
+            System.out.println("Harga Akhir = " + hargaAkhir);
+            p.setStok(jmlhBeli);
+        }
+        System.out.println("Stok terbaru = " + p.getStok());
+    }
 }

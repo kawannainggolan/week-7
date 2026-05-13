597ab5c65855f946a555f8a4d07017bfc57b5167
kawannainggolan <kawannainggolan19@gmail.com>
2026-05-13 10:16:41 +0700

    oveerride hitubgHargaAkhir diskon

--- Elektronik.java
+++ Elektronik.java
@@ -4,5 +4,11 @@ public class Elektronik extends produk {
         super(nama, harga, stok);
         this.masaGaransi = masaGaransi;
     }
+
+    @Override
+    public double hitungHargaAkhir(int jmlhBeli) {
+        System.out.println("Total Harga=" + hitungTotalHarga(jmlhBeli));
+        return hitungTotalHarga(jmlhBeli) * 0.85;
+    }
 }
 
